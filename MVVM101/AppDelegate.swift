155bf700ecb3fc8e528972c0a0a05955c385e867
Auttapon Duangumporn <guyAuttapon@CPAAP1500009.local>
2019-10-17 16:04:03 +0700

    init proj

---
+++ MVVM101/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  MVVM101
+//
+//  Created by Auttapon Duangumporn on 19/9/2562 BE.
+//  Copyright © 2562 mm. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
